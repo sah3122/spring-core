e7ad88bfac6f0dfd3bab8e916bb92c392476c19f
dongchul <sorkehdcjf@gmail.com>
2021-03-27 23:57:10 +0900

    feat: AppConfig 리팩토링

--- src/main/java/me/study/core/AppConfig.java
+++ src/main/java/me/study/core/AppConfig.java
@@ -1,6 +1,8 @@
 package me.study.core;
 
+import me.study.core.discount.DiscountPolicy;
 import me.study.core.discount.FixDiscountPolicy;
+import me.study.core.member.MemberRepository;
 import me.study.core.member.MemberService;
 import me.study.core.member.MemberServiceImpl;
 import me.study.core.member.MemoryMemberRepository;
@@ -10,10 +12,19 @@ import me.study.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @org.jetbrains.annotations.NotNull
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
