b2db8bcef9e9f16d6c3797915519945413020807
dongchul <sorkehdcjf@gmail.com>
2021-03-28 22:32:55 +0900

    feat: 할인 정책 변경 (OCP, DIP)

--- src/main/java/me/study/core/AppConfig.java
+++ src/main/java/me/study/core/AppConfig.java
@@ -2,6 +2,7 @@ package me.study.core;
 
 import me.study.core.discount.DiscountPolicy;
 import me.study.core.discount.FixDiscountPolicy;
+import me.study.core.discount.RateDiscountPolicy;
 import me.study.core.member.MemberRepository;
 import me.study.core.member.MemberService;
 import me.study.core.member.MemberServiceImpl;
@@ -23,8 +24,7 @@ public class AppConfig {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    @org.jetbrains.annotations.NotNull
     private DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
