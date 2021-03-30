428c6ab28b2f9dc96e6263e6c6fd1088a0097ae6
dean <sah3122@naver.com>
2021-03-30 22:01:02 +0900

    test: 모든 빈 조회

---
+++ src/test/java/me/study/core/ApplicationContextInfoTest.java
@@ -0,0 +1,23 @@
+package me.study.core;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+    ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+
+    @DisplayName("모든 빈 출력")
+    @Test
+    void findAllBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("bean = " + bean);
+        }
+    }
+    
+}
