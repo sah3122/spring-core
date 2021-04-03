c0c91dfd91d2bc974be3491b94e8217d532960a3
dongchul <sorkehdcjf@gmail.com>
2021-04-03 23:46:06 +0900

    feat: 싱글톤 패턴

---
+++ src/test/java/me/study/core/singleton/SingletonService.java
@@ -0,0 +1,13 @@
+package me.study.core.singleton;
+
+public class SingletonService {
+    private static final SingletonService singletonService = new SingletonService();
+
+    public static SingletonService getInstance() {
+        return singletonService;
+    }
+
+    private SingletonService() {
+        
+    }
+}
