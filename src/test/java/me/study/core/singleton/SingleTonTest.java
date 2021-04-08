0e5e1f40d4e7d8a5111210148697c5a408477910
dongchul <sorkehdcjf@gmail.com>
2021-04-08 23:45:44 +0900

    feat: 싱글톤 컨테이너 테스트

---
+++ src/test/java/me/study/core/singleton/SingleTonTest.java
@@ -0,0 +1,22 @@
+package me.study.core.singleton;
+
+import me.study.core.AppConfig;
+import me.study.core.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingleTonTest {
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        assertThat(memberService1).isSameAs(memberService2);
+    }
+}
